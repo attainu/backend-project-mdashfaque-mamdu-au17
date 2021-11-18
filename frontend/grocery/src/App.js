1c6b544e2ddacc835831f514b0458d760920934e
mdashfaque <ashfaquemamdu666@gmail.com>
2021-11-18 19:22:02 +0530

    product component and basic routes added

--- frontend/grocery/src/App.js
+++ frontend/grocery/src/App.js
@@ -1,33 +1,32 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import './App.css';
-import Navbar from './component/Navbar'
-import Product from './component/product'
-
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen/HomeScreen';
+import ProductScreen from './screens/ProductScreen/ProductScreen';
 function App() {
   return (
-    <>
-    <Navbar/>,
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    <Product/>
-    </>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              GroceryBazar
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
---
+++ frontend/grocery/src/screens/ProductScreen/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProductScreen() {
+  return (
+    <div>
+      <h1>This page is about details of Product</h1>
+    </div>
+  );
+}
+
+export default ProductScreen;
