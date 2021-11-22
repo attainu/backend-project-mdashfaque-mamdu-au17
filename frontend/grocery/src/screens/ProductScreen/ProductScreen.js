13329d9797fc30c16208c9a123cce799461f8149
mdashfaque <ashfaquemamdu666@gmail.com>
2021-11-22 18:52:02 +0530

    added redux to homescreen

--- frontend/grocery/src/screens/ProductScreen/ProductScreen.js
+++ frontend/grocery/src/screens/ProductScreen/ProductScreen.js
@@ -1,54 +1,69 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import data from '../../data';
+import { detailsProduct } from '../../actions/productActions';
+import LoadingBox from '../../component/LoadingBox/LoadingBox';
+import MessageBox from '../../component/MessageBox/MessageBox';
 import './ProductScreen.css';
 
 function ProductScreen(props) {
-  const product = data.products.find((x) => x._id === props.match.params.id);
+  const dispatch = useDispatch();
+  const productId = props.match.params.id;
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
 
-  if (!product) {
-    return <div>Product Not Found</div>;
-  }
+  useEffect(() => {
+    dispatch(detailsProduct(productId));
+  }, [dispatch, productId]);
+
   return (
     <div>
-      <Link to="/">Go Back</Link>
-      <div className="row top">
-        <div className="col-2">
-          <img className="large" src={product.image} alt={product.name} />
-        </div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div>
+          <Link to="/">Go Back</Link>
+          <div className="row top">
+            <div className="col-2">
+              <img className="large" src={product.image} alt={product.name} />
+            </div>
 
-        <div className="col-1">
-          <ul>
-            <li>
-              <h1>{product.name}</h1>
-            </li>
+            <div className="col-1">
+              <ul>
+                <li>
+                  <h1>{product.name}</h1>
+                </li>
 
-            <li>Price: ₹{product.price}</li>
+                <li>Price: ₹{product.price}</li>
 
-            <li>
-              Description:
-              <p>{product.description}</p>
-            </li>
-          </ul>
-        </div>
+                <li>
+                  Description:
+                  <p>{product.description}</p>
+                </li>
+              </ul>
+            </div>
 
-        <div className="col-1">
-          <div className="card card-body">
-            <ul>
-              <li>
-                <div className="row">
-                  <div>Price</div>
-                  <div className="price">₹{product.price}</div>
-                </div>
-              </li>
+            <div className="col-1">
+              <div className="card card-body">
+                <ul>
+                  <li>
+                    <div className="row">
+                      <div>Price</div>
+                      <div className="price">₹{product.price}</div>
+                    </div>
+                  </li>
 
-              <li>
-                <button className="primary block">Add to Cart</button>
-              </li>
-            </ul>
+                  <li>
+                    <button className="primary block">Add to Cart</button>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
