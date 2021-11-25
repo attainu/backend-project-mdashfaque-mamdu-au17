912a5404d653d8a8f1fce1a7c97b752d461b3418
mdashfaque <ashfaquemamdu666@gmail.com>
2021-11-25 18:06:37 +0530

    user signup updated

--- frontend/grocery/src/screens/SignUpScreen/SignUpScreen.js
+++ frontend/grocery/src/screens/SignUpScreen/SignUpScreen.js
@@ -1,26 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { login } from '../../actions/userActions';
+import { signup } from '../../actions/userActions';
 import LoadingBox from '../../component/LoadingBox/LoadingBox';
 import MessageBox from '../../component/MessageBox/MessageBox';
-import './LoginScreen.css';
 
-function LoginScreen(props) {
+function SignUpScreen(props) {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const redirect = props.location.search
     ? props.location.search.split('=')[1]
     : '/';
 
-  const userLogin = useSelector((state) => state.userLogin);
-  const { userInfo, loading, error } = userLogin;
+  const userSignup = useSelector((state) => state.userSignup);
+  const { userInfo, loading, error } = userSignup;
 
   const dispatch = useDispatch();
   const submitHandler = (e) => {
     e.preventDefault();
-    dispatch(login(email, password));
+    if (password !== confirmPassword) {
+      alert('Password does not match');
+    } else {
+      dispatch(signup(name, email, password, confirmPassword));
+    }
   };
 
   useEffect(() => {
@@ -33,10 +38,21 @@ function LoginScreen(props) {
     <div>
       <form className="form" onSubmit={submitHandler}>
         <div>
-          <h1>Log In</h1>
+          <h1>Sign Up</h1>
         </div>
         {loading && <LoadingBox></LoadingBox>}
         {error && <MessageBox variant="danger">{error}</MessageBox>}
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Enter name"
+            required
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+        </div>
+
         <div>
           <label htmlFor="email">Email</label>
           <input
@@ -60,16 +76,28 @@ function LoginScreen(props) {
         </div>
 
         <div>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            id="confirmpassword"
+            placeholder="confirm password"
+            required
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></input>
+        </div>
+
+        <div>
           <label />
           <button className="primary" type="submit">
-            Log In
+            Sign Up
           </button>
         </div>
 
         <div>
           <label />
           <div>
-            New customer? <Link to="/signup">Create your account</Link>
+            Already have account?{' '}
+            <Link to={`/login?redirect=${redirect}`}>Log In</Link>
           </div>
         </div>
       </form>
@@ -77,4 +105,4 @@ function LoginScreen(props) {
   );
 }
 
-export default LoginScreen;
+export default SignUpScreen;
