4c5ef86c76b4780c3ab2e3b9028194ef67860d60
DHARM SINGH <dharmjat45@gmail.com>
2021-11-25 11:21:32 +0530

    push some file

--- frontend/grocery/src/App.js
+++ frontend/grocery/src/App.js
@@ -1,32 +1,33 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen/HomeScreen';
-import ProductScreen from './screens/ProductScreen/ProductScreen';
+// import ReactDOM from 'react-dom';
+import './App.css';
+import Navbar from './component/Navbar/Navbar'
+import Product from './component/Product/product'
+
 function App() {
   return (
-    <BrowserRouter>
-      <div className="grid-container">
-        <header className="row">
-          <div>
-            <a className="brand" href="/">
-              GroceryBazar
-            </a>
-          </div>
-          <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
-          </div>
-        </header>
-
-        <main>
-          <Route path="/product/:id" component={ProductScreen}></Route>
-          <Route path="/" component={HomeScreen} exact></Route>
-        </main>
-
-        <footer className="row center">All right reserved</footer>
-      </div>
-    </BrowserRouter>
+    <>
+    <Navbar/>,
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    <Product/>
+    </>
   );
 }
 
 export default App;
+
