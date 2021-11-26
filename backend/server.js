a7d6dc5403dfb10dd821d7040d67bd5c23a5ffaf
mdashfaque <ashfaquemamdu666@gmail.com>
2021-11-26 10:21:14 +0530

    backend correction

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,6 @@ app.use('/api/users', userRouter);
 
 app.use('/api/products', productRouter);
 
-app.get('/', (req, res) => {
-  res.send('This is the Home page of API');
-});
-
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
   next();
