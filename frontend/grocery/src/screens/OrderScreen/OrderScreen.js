d601f7515e3ebd958cd568aa1f49e72a57dc7ab0
mdashfaque <ashfaquemamdu666@gmail.com>
2021-11-27 22:35:48 +0530

    changes

--- frontend/grocery/src/screens/OrderScreen/OrderScreen.js
+++ frontend/grocery/src/screens/OrderScreen/OrderScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 function OrderScreen() {
   return (
     <div>
       <h1>Your order is placed</h1>
+      <Link to="/">Continue Shopping</Link>
     </div>
   );
 }
